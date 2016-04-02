8bf7a973bf1626f4ba01ab0d70c95ab274d73941
Stephen Fager <sfagerca82@yahoo.com>
2016-04-01 22:33:40 -0400

    Created Start-VMsByResourceGroup.ps1 in AzureAutomation.

---
+++ runbooks/Start-VMsByResourceGroup.ps1
@@ -0,0 +1,25 @@
+Param (
+	[Parameter(Mandatory=$true)]
+	[String]
+	$AzureResourceGroup
+)
+
+#The name of the Automation Credential Asset
+$CredentialAssetName = "Automator"
+
+#Get the credential with the above name from the Automation Asset store
+$Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+if (!$Cred) {
+	Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
+}
+
+#Connect to Azure Account
+Add-AzureRmAccount -Credential $Cred
+
+Write-Output "Starting VMs in '$($AzureResourceGroup)' resource group"
+	
+Get-AzureRmVM -ResourceGroupName $AzureResourceGroup | ForEach-Object {
+	
+	Write-Output "Starting '$($_.Name)' ..."
+	Stop-AzureRmVM -ResourceGroupName $AzureResourceGroup -Name $_.Name -Force
+}
